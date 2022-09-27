b80ca6c2d60142eea2d4372a8ac576259c01ce1a
Ilya.Gurenkov <ilya.gurenkov163@gmail.com>
2022-09-27 20:45:31 +0400

    Добавление директории и файла ДЗ 007 и blackjack

---
+++ DZ_OOP_007/BlackJack/BlackJack.cpp
@@ -0,0 +1,427 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <Windows.h>
+
+using namespace std;
+
+class Card
+{
+    friend ostream& operator << (ostream& out, const Card& card);
+
+public:
+    enum rank
+    {
+        ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
+        JACK, QUEEN, KING
+    };
+
+    enum suit { CLUBS, DIAMONDS, HEARTS, SPADES };
+
+    Card(rank r, suit s, bool IsFaceUp) : m_Rank(r), m_Suit(s), m_IsFaceUp(IsFaceUp)
+    {
+
+    }
+
+    void Flip()
+    {
+        m_IsFaceUp = !(m_IsFaceUp);
+    }
+    int GetValue()
+    {
+        int value = 0;
+        if (m_IsFaceUp)
+        {
+            value = m_Rank;
+            if (value > 10)
+            {
+                value = 10;
+            }
+        }
+        return value;
+    }
+private:
+    rank m_Rank;
+    suit m_Suit;
+    bool m_IsFaceUp;
+};
+
+ostream& operator<< (ostream& out, const Card& card)
+{
+    const string RANKS[] = { "0", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+    const string SUITS[] = { "C", "D", "H", "S" };
+
+    if (card.m_IsFaceUp)
+    {
+        out << "Card: " << RANKS[card.m_Rank] << SUITS[card.m_Suit];
+    }
+    else
+    {
+        out << "XX";
+    }
+    return out;
+}
+
+class Hand
+{
+protected:
+
+    vector<Card*> m_Cards;
+
+public:
+
+    Hand()
+    {
+        m_Cards.reserve(7);
+    }
+
+    virtual ~Hand()
+    {
+        Clear();
+    }
+
+    void Add(Card* pCard)
+    {
+        m_Cards.push_back(pCard);
+    }
+
+    void Clear()
+    {
+        vector<Card*>::iterator iter = m_Cards.begin();
+        for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter)
+        {
+            delete* iter;
+            *iter = 0;
+        }
+        
+        m_Cards.clear();
+    }
+
+    int GetTotal()
+    {
+        int sum = 0;
+        vector<Card*>::const_iterator iter;
+
+        for (iter = m_Cards.begin(); iter != m_Cards.end(); iter++)
+        {
+            sum += (*iter)->GetValue();
+        }
+
+        bool containsAce = false;
+        for (iter = m_Cards.begin(); iter != m_Cards.end(); iter++)
+        {
+            if ((*iter)->GetValue() == Card::ACE)
+            {
+                containsAce = true;
+            }
+        }
+
+        if (containsAce && sum <= 11)
+        {
+            sum += 10;
+        }
+        return sum;
+    }
+};
+
+class GenericPlayer : public Hand
+{
+protected:
+    string m_Name;
+    friend ostream& operator << (ostream& out, GenericPlayer& genericplayer);
+
+public:
+
+    GenericPlayer(const string& name = "") : m_Name(name) {};
+
+    virtual ~GenericPlayer() {};
+
+    virtual bool IsHitting() = 0;
+
+    bool IsBusted()
+    {
+        return (GetTotal() > 21);
+    }
+    void Bust()
+    {
+        cout << "Player " << m_Name << " Bust!";
+    }
+};
+
+
+ostream& operator << (ostream& out, GenericPlayer& genericplayer)
+{
+    out << genericplayer.m_Name << ":   ";
+    vector<Card*>::const_iterator pCard;
+    if (!genericplayer.m_Cards.empty())
+    {
+        for (pCard = genericplayer.m_Cards.begin();
+            pCard != genericplayer.m_Cards.end();
+            ++pCard)
+        {
+            out << *(*pCard) << "   ";
+        }
+        if (genericplayer.GetTotal() != 0)
+        {
+            cout << "(" << genericplayer.GetTotal() << ")";
+        }
+        else
+        {
+            out << "<EMPTY>";
+        }
+    }
+    return out;
+}
+
+class Player : public GenericPlayer
+{
+public:
+
+    Player(const string& name) : GenericPlayer(name) {};
+
+    virtual ~Player() {};
+
+    bool IsHitting() override
+    {
+        cout << m_Name << "Want you a Card? Press Y (Yes) or another (No): ";
+        char addcard;
+        cin >> addcard;
+        if (addcard == 'Y' || addcard == 'y')
+        {
+            return (addcard == 'y' || addcard == 'Y');
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Win() const
+    {
+        cout << m_Name << " Won!";
+    }
+    void Lose() const
+    {
+        cout << m_Name << " Lose!";
+    }
+    void Push() const
+    {
+        cout << m_Name << " Push!";
+    }
+};
+
+class House : public GenericPlayer
+{
+public:
+    House(const string& name = "House") : GenericPlayer(name) {};
+    virtual ~House() {};
+
+    virtual bool IsHitting() override
+    {
+        return (GetTotal() <= 16);
+    }
+
+    void FlipFirstCard()
+    {
+        if (!(m_Cards.empty()))
+        {
+            m_Cards[0]->Flip();
+        }
+        else
+        {
+            cout << "No card to flip!" << endl;
+        }
+    }
+};
+
+class Deck : public Hand
+{
+public:
+    Deck()
+    {
+        m_Cards.reserve(52);
+        Populate();
+    };
+    virtual ~Deck() {};
+
+    void Populate()
+    {
+        Clear();
+
+        for (int s = Card::CLUBS; s <= Card::SPADES; ++s)
+        {
+            for (int r = Card::ACE; r <= Card::KING; ++r)
+            {
+                Add(new Card(static_cast<Card::rank>(r), static_cast<Card::suit>(s), true));
+            }
+        }
+    }
+
+    void Shuffle()
+    {
+        random_shuffle(m_Cards.begin(), m_Cards.end());
+    }
+
+    void Deal(Hand& aHand)
+    {
+        if (!m_Cards.empty())
+        {
+            aHand.Add(m_Cards.back());
+            m_Cards.pop_back();
+        }
+        else
+        {
+            cout << "Out of cards. Unable to deal.";
+        }
+    }
+    void AdditionalCards(GenericPlayer& aGenericPlayer)
+    {
+        cout << endl;
+
+        while (!(aGenericPlayer.IsBusted()) && aGenericPlayer.IsHitting())
+        {
+            Deal(aGenericPlayer);
+            cout << aGenericPlayer << endl;
+            if (aGenericPlayer.IsBusted())
+            {
+                aGenericPlayer.Bust();
+            }
+        }
+    }
+};
+
+class Game
+{
+private:
+    House m_House;
+    Deck m_Deck;
+    vector<Player> m_Players;
+public:
+    Game(const vector<string>& names)
+    {
+        vector<string>::const_iterator pName;
+        for (pName = names.begin(); pName != names.end(); ++pName)
+        {
+            m_Players.push_back(Player(*pName));
+        }
+
+        srand(static_cast<unsigned int>(time(0)));
+        m_Deck.Populate();
+        m_Deck.Shuffle();
+    }
+
+    ~Game() {};
+
+    void Play()
+    {
+        vector<Player>::iterator pPlayer;
+        for (int i = 0; i < 2; ++i)
+        {
+            for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+            {
+                m_Deck.Deal(*pPlayer);
+            }
+            m_Deck.Deal(m_House);
+        }
+
+        m_House.FlipFirstCard();
+
+        for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+        {
+            cout << *pPlayer << endl;
+        }
+        cout << m_House << endl;
+
+        for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+        {
+            m_Deck.AdditionalCards(*pPlayer);
+        }
+
+        m_House.FlipFirstCard();
+        cout << endl << m_House;
+
+        m_Deck.AdditionalCards(m_House);
+
+        if (m_House.IsBusted())
+        {
+            for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+            {
+                if (!(pPlayer->IsBusted()))
+                {
+                    pPlayer->Win();
+                }
+            }
+        }
+        else
+        {
+            for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+            {
+                if (!(pPlayer->IsBusted()))
+                {
+                    if (pPlayer->GetTotal() > m_House.GetTotal())
+                    {
+                        pPlayer->Win();
+                    }
+                    else if (pPlayer->GetTotal() < m_House.GetTotal())
+                    {
+                        pPlayer->Lose();
+                    }
+                    else
+                    {
+                        pPlayer->Push();
+                    }
+                }
+            }
+        }
+        for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+        {
+            pPlayer->Clear();
+        }
+        m_House.Clear();
+    }
+};
+
+int main()
+{    
+    setlocale(LC_ALL, "ru");
+    SetConsoleTitle(L"BlackJack");
+
+    cout << "       Welcome to Blackjack!" << endl << endl;
+    cout << "Краткие правила игры:" << endl << endl;
+    cout << "1. Главная цель игры набрать больше очков чем дилер (House) и не сделать перебор, перебор это больше 21 очка." << endl;
+    cout << "2. В начале игры выдаются по две карты игроку и дилеру, первая карта дилера скрыта." << endl;
+    cout << "3. Возьмите ещё карту, если считаете, что у вас меньше, чем у дилера." << endl;
+    cout << "4. Очки карт с цифрами равны номиналу карты, а валет, дама и король дают 10 очков, туз даёт 1 очко или 11, если не будет перебора." << endl;
+    cout << "5. Дилер будет брать карты пока у него меньше 17 очков." << endl;
+    cout << "6. Вы выиграли, если после отказа от взятия карты у вас больше очков, чем у дилера, но меньше, либо равно 21 очку, в противном случае выигрывает дилер." << endl;
+    cout << "7. Если у игрока и дилера одинаковое количество очков, то объявляется ничья." << endl << endl;
+
+    int numPlayers = 0;
+    while (numPlayers < 1 || numPlayers > 7)
+    {
+        cout << "How many players? (1 - 7): ";
+        cin >> numPlayers;
+    }
+    vector<string> names;
+    string name;
+    for (int i = 0; i < numPlayers; ++i)
+    {
+        cout << "Enter player name: ";
+        cin >> name;
+        names.push_back(name);
+    }
+    cout << endl;
+
+    Game aGame(names);
+    char again = 'y';
+    while (again != 'n' && again != 'N')
+    {
+        aGame.Play();
+        cout << endl << "Do you want to play again? (Y/N): ";
+        cin >> again;
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ DZ_OOP_007/DZ_OOP_007 (Задания 1 и 2).cpp
@@ -0,0 +1,151 @@
+﻿#include <iostream>
+#include <memory>
+
+using namespace std;
+
+/*1. Создайте класс Date с полями день, месяц, год и методами доступа к этим полям.Перегрузите оператор вывода для данного класса.
+Создайте два "умных" указателя today и date.Первому присвойте значение сегодняшней даты.Для него вызовите по отдельности методы доступа
+к полям класса Date, а также выведите на экран данные всего объекта с помощью перегруженного оператора вывода.Затем переместите ресурс,
+которым владеет указатель today в указатель date.Проверьте, являются ли нулевыми указатели today и date и выведите соответствующую информацию
+об этом в консоль.*/
+
+/*2. По условию предыдущей задачи создайте два умных указателя date1 и date2.
+• Создайте функцию, которая принимает в качестве параметра два умных указателя типа Date и сравнивает их между собой
+(сравнение происходит по датам).Функция должна вернуть более позднюю дату.
+• Создайте функцию, которая обменивает ресурсами(датами) два умных указателя, переданных в функцию в качестве параметров.
+
+Примечание: обратите внимание, что первая функция не должна уничтожать объекты, переданные ей в качестве параметров.*/
+
+class Date
+{
+protected:
+    unsigned int mDay;
+    unsigned int mMonth;
+    unsigned int mYear;
+    friend ostream& operator << (ostream& out, Date& mData);
+
+public:
+    Date(unsigned int Day, unsigned int Month, unsigned int Year) : mDay(Day), mMonth(Month), mYear(Year) {};
+
+    int SetDay() const
+    {        
+        return mDay;
+    }
+    int SetMonth() const
+    {
+        return mMonth;
+    }
+    int SetYear() const
+    {
+        return mYear;
+    }
+    friend Date PointerDate(shared_ptr<Date> date1, shared_ptr<Date> date2);
+    friend void PointerSwap(shared_ptr<Date> date1, shared_ptr<Date> date2);
+};
+
+ostream& operator << (ostream& out, Date& mData)
+{
+    return out << "   " << mData.SetDay() << "." << mData.SetMonth() << "." << mData.SetYear();
+};
+
+Date PointerDate(shared_ptr<Date> date1, shared_ptr<Date> date2)
+{
+    if (date1->mYear > date2->mYear)
+    {
+        cout << "   Past date is: " << *date1 << endl;
+        return *date1;        
+    }
+    else if (date2->mYear > date1->mYear)
+    {
+        cout << "   Past date is: " << *date2 << endl;
+        return *date2;
+    }
+    else if (date1->mMonth > date2->mMonth)
+    {
+        cout << "   Past date is: " << *date1 << endl;
+        return *date1;
+    }
+    else if (date2->mMonth > date1->mMonth)
+    {
+        cout << "   Past date is: " << *date2 << endl;
+        return *date2;
+    }
+    else if (date1->mDay > date2->mDay)
+    {
+        cout << "   Past date is: " << *date1 << endl;
+        return *date1;
+    }
+    else if (date2->mDay > date1->mDay)
+    {
+        cout << "   Past date is: " << *date2 << endl;
+        return *date2;
+    }
+    else
+    {
+        cout << "   Same date: " << *date1 << ", " << *date2 << endl;        
+    }    
+}
+
+void PointerSwap(shared_ptr<Date> date1, shared_ptr<Date> date2)
+{
+    swap(date1, date2);
+    cout << *date1 << endl << *date2 << endl;
+}
+
+int main()
+{
+    cout << endl;
+    cout << "   Exercise 1";
+    cout << endl << endl;
+
+    shared_ptr<Date> todayPtr = make_shared<Date>(25, 9, 2022);       
+
+    todayPtr->SetDay();
+    todayPtr->SetMonth();
+    todayPtr->SetYear();
+
+    cout << *todayPtr;
+    cout << endl;
+    shared_ptr<Date> datePtr = move(todayPtr);
+    cout << *datePtr;
+    cout << endl;
+    for (;;)
+    {
+        if (todayPtr == nullptr)
+        {
+            cout << "   Pointer todayPtr is empty" << endl;
+        }
+        else
+        {
+            cout << "   Pointer todayPtr is NOT empty" << endl;
+        }
+        break;
+    }
+    for (;;)
+    {
+        if (datePtr == nullptr)
+        {
+            cout << "   Pointer datePtr is empty" << endl;
+        }
+        else
+        {
+            cout << "   Pointer datePtr is NOT empty" << endl;
+        }
+        break;
+    }
+    
+    cout << endl;
+    cout << "   Exercise 2";
+    cout << endl << endl;
+
+    shared_ptr<Date> date1 = make_shared<Date>(25, 12, 2022);
+
+    shared_ptr<Date> date2 = make_shared<Date>(27, 12, 2022);
+
+    PointerDate(date1, date2);    
+    cout << endl;
+    PointerSwap(date1, date2);
+
+    return 0;
+}
+
